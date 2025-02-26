180d0e1b4ea0ab47f7afbfce2ae422fb29267684
MeriameM <meriame.moula@guepard.run>
2025-02-26 13:24:27 +0100

    Adding documentation and re-orginazing the code

--- src/application/commands/deploy.rs
+++ src/application/commands/deploy.rs
@@ -2,7 +2,17 @@ use crate::application::dto::deploy_dto::{CreateDeploymentRequest, UpdateDeploym
 use crate::application::services::deploy_service;
 use crate::structure::{CreateDeployArgs, UpdateDeployArgs};
 use anyhow::Result;
-// (Handles Command Execution)
+/// (Handles Command Execution)
+/// 
+/// 
+/// Handles the creation of a new deployment.
+///
+/// This function takes the parsed arguments for creating a deployment,
+/// constructs a `CreateDeploymentRequest`, and calls the service to create the deployment.
+///
+/// # Arguments
+///
+/// * `args` - A reference to the parsed arguments for creating a deployment.
 pub async fn create(args: &CreateDeployArgs) -> Result<()> {
     let request = CreateDeploymentRequest {
         database_provider: args.database_provider.clone(),
@@ -17,7 +27,14 @@ pub async fn create(args: &CreateDeployArgs) -> Result<()> {
     deploy_service::create_deployment(request).await?;
     Ok(())
 }
-
+// Handles the update of an existing deployment.
+///
+/// This function takes the parsed arguments for updating a deployment,
+/// constructs an `UpdateDeploymentRequest`, and calls the service to update the deployment.
+///
+/// # Arguments
+///
+/// * `args` - A reference to the parsed arguments for updating a deployment.
 pub async fn update(args: &UpdateDeployArgs) -> Result<()> {
     let request = UpdateDeploymentRequest {
         repository_name: args.repository_name.clone(),
--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,20 @@
+/// Main entry point for the CLI application.
+///
+/// This function initializes the environment by loading `.env` variables,
+/// parses the command line arguments, and executes the appropriate subcommand.
+/// If an error occurs during execution, it handles the error and exits with
+/// the appropriate exit code.
+///
+/// # Errors
+///
+/// If an error occurs during the execution of a subcommand, it will be printed
+/// to the standard error output and the application will exit with a non-zero
+/// exit code.
 use clap::Parser;
 use dotenvy::dotenv;
 use guepard_cli::application::commands::deploy;
 use guepard_cli::domain::errors::deploy_error::DeployError;
-use guepard_cli::structure::{CLI, DeployCommand, SubCommand};
-
+use guepard_cli::structure::{DeployCommand, SubCommand, CLI};
 
 #[tokio::main]
 async fn main() {
@@ -30,13 +41,24 @@ async fn main() {
         std::process::exit(exit_code);
     }
 }
-
+/// Executes the appropriate subcommand based on user input.
+///
+/// This function matches the parsed subcommands and calls the corresponding
+/// handler function to perform the requested operation.
+///
+/// # Arguments
+///
+/// * `sub_commands` - A reference to the parsed subcommands.
+///
+/// # Returns
+///
+/// * `anyhow::Result<()>` - Returns an `Ok(())` if the subcommand executes successfully,
+///   otherwise returns an error.
 async fn run(sub_commands: &SubCommand) -> anyhow::Result<()> {
     match sub_commands {
         SubCommand::Deploy(cmd) => match cmd {
             DeployCommand::Create(args) => deploy::create(args).await,
             DeployCommand::Update(args) => deploy::update(args).await,
         },
-        _ => Ok(()),
     }
 }
